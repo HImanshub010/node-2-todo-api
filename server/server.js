748299363b2b311c03400d53fc7d7b8785ce5e58
Himanshu <you@example.com>
2018-05-30 14:18:49 +0530

    adding get todo route

--- server/server.js
+++ server/server.js
@@ -21,11 +21,19 @@ app.post('/todos',(req,res)=>{
 	});
 });
 
+
+app.get('/todos',(req,res)=>{
+	Todo.find().then((todos)=>{
+      res.send({todos})
+	},(e)=>{
+       res.status(400).send(e);
+	});
+})
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
 
 //Used to configure route Post being one of them	
 //C create a resource using post HTTP method and send it as a body 
-//R
+//R 
 //
