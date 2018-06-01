25d03e739f8c571e1d4e1cf6edb3bbbac8a3cd27
Himanshu <you@example.com>
2018-06-01 17:36:26 +0530

    Add GET/todo/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var {ObjectID}=require('mongodb');
 
-
-var{mongoose}=require('./db/mongoose.js');
+var{mongoose}=require('./db/mongoose');
 var{Todo}=require('./models/todo');
 var{User}=require('./models/user');
 
@@ -28,7 +28,23 @@ app.get('/todos',(req,res)=>{
 	},(e)=>{
        res.status(400).send(e);
 	});
-})
+});
+//get/todos/123
+app.get('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+     if(!ObjectID.isValid(id)){
+     	return res.status(404).send();
+     }
+
+    Todo.findById(id).then((todo)=>{
+	  if(!todo){//occurs when the objectId is valid but no user is found with this id
+		return res.status(400).send();
+	}
+      res.send({todo});//{todo }is similar to {todo:todo}
+	}).catch((e)=>{
+		res.status(400).send();
+	});
+});
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
