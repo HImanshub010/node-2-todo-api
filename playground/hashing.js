3997c37b9a82f4b87a1b248cf291089c65612601
Himanshu <you@example.com>
2018-07-10 21:55:01 +0530

    Add /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,47 @@
+const{SHA256}=require('crypto-js');
+const jwt=require('jsonwebtoken');
+
+var data={
+	id:10
+};
+var token=jwt.sign(data,'abc123');
+
+console.log(`tken is : ${token}`); 
+
+var decoded=jwt.verify(token,'abc123');
+
+console.log('Decoded tken is   ',decoded);
+
+
+
+
+
+
+
+// var message='Helllo I am 12';
+// var hash=SHA256(message).toString();
+
+// console.log(`Message : ${message}`);
+// console.log(`Hashed Message : ${hash}`);
+
+
+// var data={
+// 	id:4
+// }
+
+// var token={
+// 	data:data,
+// 	hash:SHA256(JSON.stringify(data)+'somesecret').toString()
+// }
+
+// // token.data.id=5;
+// // token.hash=SHA256(JSON.stringify(token.data.id)+'somesecret').toString(); 	
+
+
+// var resultRes= SHA256(JSON.stringify(token.data)+'somesecret').toString()
+
+// if(token.hash===resultRes){
+// 	console.log('Data not Modified');
+// }else{
+// 	console.log('Data Modified do not trust');
+// }
--- server/server.js
+++ server/server.js
@@ -88,6 +88,21 @@ app.patch('/todos/:id',(req,res)=>{
          res.status(400).send();
      });
 });
+
+app.post('/users',(req,res)=>{
+	var body=_.pick(req.body,['email','password']);
+	var user=new User(body);
+
+	user.save().then((user)=>{
+		//res.send(user); 
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((e)=>{
+		res.status(400).send(e);
+	})
+});
+
 app.listen(port,()=>{
   console.log(`Started on port on ${port}`);
 });
