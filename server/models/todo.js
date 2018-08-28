7d0b917a2443e6d3f0f128d2c499781458d3bd19
Himanshu <you@example.com>
2018-08-29 00:04:23 +0530

    Associating todos route with the users

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,19 +2,23 @@ var mongoose=require('mongoose');
 //creating a model that will specify the items in the model(here Todo).It wil specify ther types and other related properties of items  
 var Todo=mongoose.model('Todo',{
 	text:{
-       type:String,
-       trim:true,
-       minlength:1,
-       required:true
+              type:String,
+              trim:true,
+              minlength:1,
+              required:true
 	},
 	completed:{
-       type:Boolean,
-       default:false
-	},
+              type:Boolean,
+              default:false
+       	},
 	completedAt:{
-       type:Number,
-       default:null
-	}
+              type:Number,
+              default:null
+	},
+       _creator:{
+              type:mongoose.Schema.Types.ObjectId,
+              required:true
+       }
 });
 
 module.exports={Todo};
